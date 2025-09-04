baceceae9a372cd03062424102eff19fa82241a6
Sagar Bhati <sagarbhati8837@gmail.com>
2025-09-04 19:03:54 +0530

    Load the trained model from the specified path

--- scripts/predict.py
+++ scripts/predict.py
@@ -3,7 +3,14 @@ import joblib
 import pandas as pd
 import os
 
-
+# Load model from outer directory
+model_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models', 'model.joblib')
+try:
+	model = joblib.load(model_path)
+	print("Model successfully loaded.")
+except Exception as e:
+	print(f"Model could not be loaded: {e}")
+	exit(1)
 
 
 
